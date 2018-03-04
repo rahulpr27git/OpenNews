61d02364c13ce2fa37bc31375b552b9230766157
Rahul Prasad <dev.rahulprasad@gmail.com>
2018-03-04 08:21:52 +0530

    API's added

---
+++ app/src/main/java/com/dev/rahul/opennews/api/API.java
@@ -0,0 +1,15 @@
+package com.dev.rahul.opennews.api;
+
+/**
+ * Created by rahul on 4/3/18.
+ */
+
+public interface API {
+
+    String API_KEY = "9f91e7e4cae24fbeb59363ae44e6f9c5";
+
+    String BASE_URL = "https://newsapi.org/v2/";
+    String SOURCE = BASE_URL + "sources";
+    String EVERYTHING = BASE_URL + "everything";
+    String TOP_HEADLINES = BASE_URL + "top-headlines";
+}
---
+++ app/src/main/java/com/dev/rahul/opennews/app/NewsApp.java
@@ -0,0 +1,15 @@
+package com.dev.rahul.opennews.app;
+
+import android.app.Application;
+
+/**
+ * Created by rahul on 4/3/18.
+ */
+
+public class NewsApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
